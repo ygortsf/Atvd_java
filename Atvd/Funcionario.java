d81cc17e2393a0f0b463c176a0ea29383637d813
ygortsf <ygordetarcio@gmail.com>
2025-08-22 22:26:07 +0000

    Commit atvd

---
+++ Atvd/Funcionario.java
@@ -0,0 +1,29 @@
+class Funcionario {
+
+    private int mat;
+    private String nome;
+    private Departamento dept;
+
+    public Funcionario(int mat, String nome, Departamento dept){
+        this.mat = mat;
+        this.nome = nome;
+        this.dept = dept;
+    }
+
+    public int getMat(){
+        return mat;
+    }
+
+    public Departamento getDept(){
+        return dept;
+    }
+
+    public void setMat(int mat){
+        this.mat  = mat;
+    }
+
+    public void setDept(Departamento d){
+        dept = d;
+    }
+    
+}
