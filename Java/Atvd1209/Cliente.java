9631e9f83e50cb7500df4b498e90efb068f1f8b0
ygortsf <ygordetarcio@gmail.com>
2025-09-12 23:01:11 +0000

    Sobreposicao de metodos

--- Java/Atvd1209/Cliente.java
+++ Java/Atvd1209/Cliente.java
@@ -2,11 +2,12 @@ public class Cliente {
     
     private String nome;
     private String endereco;
+    InfoCliente infoCliente;
 
     Cliente(String nome, String endereco){
         this.endereco = endereco;
         this.nome = nome;
     }
 
-    
+
 }
